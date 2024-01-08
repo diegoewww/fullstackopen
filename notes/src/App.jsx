f727df3a617c48731186283d976b9b7fce304fe9
tuvieja <105611148+n1na666@users.noreply.github.com>
2024-01-07 22:16:49 -0500

    envio de datos al servidor

---
+++ notes/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Note from './components/Note';
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState('');
+  const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+
+  console.log('render', notes.length, 'notes');
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date(),
+      important: Math.random() < 0.5,
+    };
+
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      });
+  };
+
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={(event) => addNote(event)}>
+        <input value={newNote} onChange={(event) => setNewNote(event.target.value)} />
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
